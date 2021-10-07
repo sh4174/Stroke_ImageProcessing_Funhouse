164dfd40ae397c9288b2d6c6a1e8b6d0728e3942
sh4174 <sh4174@nyu.edu>
2021-10-07 12:59:56 -0400

    first

---
+++ AcuteLesion_Segmentation/predict_unetr_lightning.py
@@ -0,0 +1,357 @@
+import os
+import shutil
+import tempfile
+import glob
+
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
+
+from monai.losses import DiceCELoss
+from monai.inferers import sliding_window_inference
+from monai.transforms import (
+    AsDiscrete,
+    AddChanneld,
+    Compose,
+    CropForegroundd,
+    LoadImaged,
+    Orientationd,
+    RandFlipd,
+    RandSpatialCropd,
+    RandShiftIntensityd,
+    ScaleIntensityd,
+    Spacingd,
+    RandRotate90d,
+    ToTensord,
+)
+
+from monai.config import print_config
+from monai.metrics import DiceMetric
+from monai.networks.nets import UNETR
+
+from monai.data import (
+    DataLoader,
+    CacheDataset,
+    load_decathlon_datalist,
+    decollate_batch,
+    list_data_collate,
+    pad_list_data_collate,
+)
+
+import torch
+import pytorch_lightning
+from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+device = torch.device(0)
+torch.backends.cudnn.benchmark = True
+print_config()
+
+
+# Output Directory
+model_dir = 'models/monai_unetr_lightning/'
+
+os.makedirs( model_dir, exist_ok=True )
+
+
+class Net(pytorch_lightning.LightningModule):
+    def __init__(self):
+        super().__init__()
+
+        self._model = UNETR(
+            in_channels=1,
+            out_channels=14,
+            img_size=(96, 96, 16),
+            feature_size=16,
+            hidden_size=768,
+            mlp_dim=3072,
+            num_heads=12,
+            pos_embed="perceptron",
+            norm_name="instance",
+            res_block=True,
+            conv_block=True,
+            dropout_rate=0.0,
+        ).to(device)
+
+        self.loss_function = DiceCELoss(to_onehot_y=True, softmax=True)
+        self.post_pred = AsDiscrete(argmax=True, to_onehot=True, num_classes=14)
+        self.post_label = AsDiscrete(to_onehot=True, num_classes=14)
+        self.dice_metric = DiceMetric(
+            include_background=False, reduction="mean", get_not_nans=False
+        )
+        self.best_val_dice = 0
+        self.best_val_epoch = 0
+        self.max_epochs = 2000
+        self.check_val = 50
+        self.warmup_epochs = 1
+        self.metric_values = []
+        self.epoch_loss_values = []
+
+    def forward(self, x):
+        return self._model(x)
+
+    def prepare_data(self):
+        # prepare data
+        # Data Directories
+        img_dir = '/home/shong/Research/LesionSegmentation/Data/Training_Resized/DWI/'
+        seg_dir = '/home/shong/Research/LesionSegmentation/Data/Training_Resized/Lesion/'
+
+        images = sorted(glob.glob(os.path.join(img_dir, "*.nii.gz")))
+        segs = sorted(glob.glob(os.path.join(seg_dir, "*.nii.gz")))
+
+        print( "========================================" )
+        print( "len images" )
+        print( len( images ) )
+        print( "========================================" )
+
+        train_files = [
+            {"image": img, "label": seg} for img, seg in zip(images[100:], segs[100:])
+        ]
+        print( "========================================" )
+        print( "len train files" )
+        print( len( train_files ) )
+        print( "========================================" )
+
+        # Data Directories
+        # pred_img_dir = '/home/shong/Research/LesionSegmentation/Data/Validation_Resized/DWI/'
+        # pred_seg_dir = '/home/shong/Research/LesionSegmentation/Data/Validation_Resized/Lesion/'
+
+        # val_images = sorted(glob.glob(os.path.join(pred_img_dir, "*.nii.gz")))
+        # val_segs = sorted(glob.glob(os.path.join(pred_seg_dir, "*.nii.gz")))
+
+        # val_files = [
+        #     {"image": img, "label": seg} for img, seg in zip(val_images[:30], val_segs[:30])
+        # ]
+        val_files = [
+            {"image": img, "label": seg} for img, seg in zip(images[:100], segs[:100])
+        ]
+
+        train_transforms = Compose(
+            [
+                LoadImaged(keys=["image", "label"]),
+                AddChanneld(keys=["image", "label"]),
+                ScaleIntensityd(
+                    keys=["image"],
+                ),
+                CropForegroundd(keys=["image", "label"], source_key="image"),
+                RandSpatialCropd(
+                    keys=["image", "label"],
+                    roi_size=(96, 96, 16),
+                    random_size=False,
+                ),
+                RandFlipd(
+                    keys=["image", "label"],
+                    spatial_axis=[0],
+                    prob=0.10,
+                ),
+                RandShiftIntensityd(
+                    keys=["image"],
+                    offsets=0.10,
+                    prob=0.50,
+                ),
+                # CropForegroundd(keys=["image", "label"], source_key="image"),
+                ToTensord(keys=["image", "label"]),
+            ]
+        )
+        val_transforms = Compose(
+            [
+                LoadImaged(keys=["image", "label"]),
+                AddChanneld(keys=["image", "label"]),
+                ScaleIntensityd(
+                    keys=["image"],
+                ),
+                # CropForegroundd(keys=["image", "label"], source_key="image"),
+                ToTensord(keys=["image", "label"]),
+            ]
+        )
+
+        self.train_ds = CacheDataset(
+            data=train_files,
+            transform=train_transforms,
+            cache_num=24,
+            cache_rate=1.0,
+            num_workers=1,
+        )
+        self.val_ds = CacheDataset(
+            data=val_files,
+            transform=val_transforms,
+            cache_num=6,
+            cache_rate=1.0,
+            num_workers=1,
+        )
+
+
+    def train_dataloader(self):
+        train_loader = torch.utils.data.DataLoader(
+            self.train_ds,
+            batch_size=8,
+            shuffle=True,
+            num_workers=8,
+            pin_memory=True,
+            collate_fn=list_data_collate,
+        )
+        return train_loader
+
+    def val_dataloader(self):
+        val_loader = torch.utils.data.DataLoader( self.val_ds, batch_size=1, shuffle=False, num_workers=4, pin_memory=True )
+        return val_loader
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW( self._model.parameters(), lr=1e-4, weight_decay=1e-5 )
+        return optimizer
+
+
+    def training_step(self, batch, batch_idx):
+        images, labels = (batch["image"].cuda(), batch["label"].cuda())
+        output = self.forward(images)
+        loss = self.loss_function(output, labels)
+        tensorboard_logs = {"train_loss": loss.item()}
+        return {"loss": loss, "log": tensorboard_logs}
+
+
+    def training_epoch_end(self, outputs):
+        avg_loss = torch.stack([x["loss"] for x in outputs]).mean()
+        self.epoch_loss_values.append(avg_loss.detach().cpu().numpy())
+
+    def validation_step(self, batch, batch_idx):
+        images, labels = batch["image"], batch["label"]
+        roi_size = (96, 96, 16)
+        sw_batch_size = 8
+        outputs = sliding_window_inference(
+            images, roi_size, sw_batch_size, self.forward
+        )
+        loss = self.loss_function(outputs, labels)
+        outputs = [self.post_pred(i) for i in decollate_batch(outputs)]
+        labels = [self.post_label(i) for i in decollate_batch(labels)]
+        self.dice_metric(y_pred=outputs, y=labels)
+        return {"val_loss": loss, "val_number": len(outputs)}
+
+
+    def validation_epoch_end(self, outputs):
+        val_loss, num_items = 0, 0
+        for output in outputs:
+            val_loss += output["val_loss"].sum().item()
+            num_items += output["val_number"]
+        mean_val_dice = self.dice_metric.aggregate().item()
+        self.dice_metric.reset()
+        mean_val_loss = torch.tensor(val_loss / num_items)
+        tensorboard_logs = {
+            "val_dice": mean_val_dice,
+            "val_loss": mean_val_loss,
+        }
+        if mean_val_dice > self.best_val_dice:
+            self.best_val_dice = mean_val_dice
+            self.best_val_epoch = self.current_epoch
+        print(
+            f"current epoch: {self.current_epoch} "
+            f"current mean dice: {mean_val_dice:.4f}"
+            f"\nbest mean dice: {self.best_val_dice:.4f} "
+            f"at epoch: {self.best_val_epoch}"
+        )
+        self.metric_values.append(mean_val_dice)
+        return {"log": tensorboard_logs}
+
+
+
+# initialise the LightningModule
+net = Net()
+
+
+'''
+# set up checkpoints
+checkpoint_callback = ModelCheckpoint(dirpath=model_dir, filename="best_metric_model")
+
+# initialise Lightning's trainer.
+trainer = pytorch_lightning.Trainer(
+    gpus=[0],
+    max_epochs=net.max_epochs,
+    # max_steps=100,
+    # limit_train_batches=100,
+    check_val_every_n_epoch=net.check_val,
+    callbacks=checkpoint_callback,
+    default_root_dir=model_dir,
+    log_every_n_steps=20,
+)
+
+
+# train
+trainer.fit(net)
+
+eval_num = 1
+plt.figure("train", (12, 6))
+plt.subplot(1, 2, 1)
+plt.title("Iteration Average Loss")
+x = [eval_num * (i + 1) for i in range(len(net.epoch_loss_values))]
+y = net.epoch_loss_values
+plt.xlabel("Iteration")
+plt.plot(x, y)
+plt.subplot(1, 2, 2)
+plt.title("Val Mean Dice")
+x = [eval_num * (i + 1) for i in range(len(net.metric_values))]
+y = net.metric_values
+plt.xlabel("Iteration")
+plt.plot(x, y)
+plt.show()
+'''
+
+# Prediction / Validation
+case_num = 4
+net.load_from_checkpoint(os.path.join(model_dir, "best_metric_model-v1.ckpt"))
+net.eval()
+net.to(device)
+
+# img_dir = '/home/shong/Research/LesionSegmentation/Data/Validation_Resized/DWI/'
+# seg_dir = '/home/shong/Research/LesionSegmentation/Data/Validation_Resized/Lesion/'
+
+img_dir = '/home/shong/Research/LesionSegmentation/Data/Training_Resized/DWI/'
+seg_dir = '/home/shong/Research/LesionSegmentation/Data/Training_Resized/Lesion/'
+
+
+images = sorted(glob.glob(os.path.join(img_dir, "*.nii.gz")))
+segs = sorted(glob.glob(os.path.join(seg_dir, "*.nii.gz")))
+
+val_files = [
+    {"image": img, "label": seg} for img, seg in zip(images[:100], segs[:100])
+]
+val_transforms = Compose(
+    [
+        LoadImaged(keys=["image", "label"]),
+        AddChanneld(keys=["image", "label"]),
+        ScaleIntensityd(
+            keys=["image"],
+        ),
+        # CropForegroundd(keys=["image", "label"], source_key="image"),
+        ToTensord(keys=["image", "label"]),
+    ]
+)
+
+val_ds = CacheDataset(
+    data=val_files,
+    transform=val_transforms,
+    cache_num=6,
+    cache_rate=1.0,
+    num_workers=1,
+)
+
+
+with torch.no_grad():
+    img = val_ds[4]["image"]
+    label = val_ds[4]["label"]
+    val_inputs = torch.unsqueeze(img, 1).cuda()
+    val_labels = torch.unsqueeze(label, 1).cuda()
+    val_outputs = sliding_window_inference(
+        val_inputs, (96, 96, 16), 8, net.forward, overlap=0.8
+    )
+    plt.figure("check", (18, 6))
+    plt.subplot(1, 3, 1)
+    plt.title(f"image")
+    plt.imshow(val_inputs.cpu().numpy()[0, 0, :, :, 16], cmap="gray")
+    plt.subplot(1, 3, 2)
+    plt.title(f"label")
+    plt.imshow(val_labels.cpu().numpy()[0, 0, :, :, 16])
+    plt.subplot(1, 3, 3)
+    plt.title(f"output")
+    plt.imshow(
+        torch.argmax(val_outputs, dim=1).detach().cpu()[0, :, :, 16]
+    )
+    plt.show()
---
+++ README.md
@@ -0,0 +1,28 @@
+# Stroke Image Processing Funhouse
+
+This repository will contain the collection of image processing pipelines that are widely used in stroke research.
+
+The goal of this project is to provide the end-to-end image processing AI/ML pipeline of stroke image processing that can be used without the deep understanding of AI/ML. The pipeline will include: 
+
+- Acute Lesion Segmentation with DWIs 
+
+- White Matter Hyperintensity Segmentation with T2 FLAIR Images
+
+- Spatial Matching of DWIs/FLAIRs to Brain Territories/Regions
+
+- And more to come! 
+
+
+The framework is powered by the MONAI framework (https://monai.io). 
+
+# Note 
+
+This git is actively under development. We will add appropriate references when it goes public.
+
+# Current Development (10/07/2021) 
+
+Currently testing/developing acute lesion segmentation framework with DWIs.
+
+# Contact 
+
+Please contact Sungmin Hong (JPK Stroke Research Center, MGH/HMS) via email shong20@mgh.harvard.edu if you have questions or would like to contribute. 
